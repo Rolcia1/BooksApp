a932d3dc2815b925c40387e867e0f24766d80dea
Karolina Zioło <karolina.ziolo@gmail.com>
2021-04-17 14:54:23 +0200

    Add 9.2 all

--- src/js/script.js
+++ src/js/script.js
@@ -45,13 +45,33 @@
     render(){
 
       for(let book of dataSource.books){
-
+        book.ratingBgc = this.determineRatingBgc(book.rating);
+        book.ratingWidth = book.rating * 10;
         const generatedHTML = templates.bookTemplate(book);
         const element = utils.createDOMFromHTML(generatedHTML);
         this.booksList.appendChild(element);
       }
 
         
+    }
+
+    determineRatingBgc(rating){
+      let color1 = '#fefcea';
+      let color2 = '##f1da36';
+
+      if (rating > 6 && rating <= 8) {
+        color1 = '#b4df5b';
+        color2 = '#b4df5b';
+    } else if(rating > 8 && rating <= 9) {
+        color1 = '#299a0b';
+        color2 = '#299a0b';
+    } else if(rating > 9) {
+        color1 = '#ff0084';
+        color2 = '#ff0084';
+    }
+
+
+      return `linear-gradient(to bottom, ${color1} 0%, ${color2} 100%);`
     }
 
 
@@ -79,23 +99,25 @@
                 
       });
 
-      this.filterWrapper.addEventListener('click', function(event){
+      this.filterWrapper.addEventListener('change', function(event){
         event.preventDefault();
         const clickedForm = event.target;
-        if((clickedForm.type === 'checkbox' && clickedForm.tagName === 'INPUT' && clickedForm.name === 'filter'){
-          if (clickedForm.checked == true) {
+        if(clickedForm.type === 'checkbox' && clickedForm.tagName === 'INPUT' && clickedForm.name === 'filter'){
+          if (clickedForm.checked) {
             thisBooksApp.filters.push(clickedForm.value);
           } else {
-            this.filterWrapper.filters.splice(thisBooksApp.filters.indexOf(clickedForm.value), 1);
+            thisBooksApp.filters.splice(thisBooksApp.filters.indexOf(clickedForm.value), 1);
           }
         }
-        this.filterWrapper.filterBooks();
+        thisBooksApp.filterBooks();
       });
+
+    }
 
       filterBooks() {
         const thisBooksApp = this;
 
-        for(let book of thisBooksApp.data){
+        for(let book of dataSource.books){
           let shouldBeHidden = false;
       
           for(const filter of thisBooksApp.filters){
@@ -119,7 +141,7 @@
       
 
 
-    }
+    
    
 
   }
